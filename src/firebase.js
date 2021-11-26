07fbdf3386da76580812861e635565e79981dbb1
anuragarwalkar <anuragarwalkar@ehrdata.com>
2021-11-26 23:36:22 +0530

    added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBH3VPnT_sZ3QkeIMNDIaNL0wxctdYRgMA",
+    authDomain: "facebook-clone-933a3.firebaseapp.com",
+    projectId: "facebook-clone-933a3",
+    storageBucket: "facebook-clone-933a3.appspot.com",
+    messagingSenderId: "638080385380",
+    appId: "1:638080385380:web:62e158ccb753114ef5ba71",
+    measurementId: "G-3H6YX3RBG1"
+  };
