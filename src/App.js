c0d6d7c40507814c131879d2e611f1d65d89f8b0
anuragarwalkar <anuragarwalkar@ehrdata.com>
2021-11-27 17:50:04 +0530

    added widgets comp

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.scss";
 import Feed from "./Feed/Feed";
 import Header from "./Header/Header";
 import Sidebar from "./Sidebar/Sidebar";
+import Widgets from "./Widget/Widgets";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <div className="app__body">
         <Sidebar />
         <Feed />
-        {/* Widgets */}
+        <Widgets />
       </div>
     </div>
   );
