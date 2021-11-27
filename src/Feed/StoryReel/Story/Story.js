c3435db461ba62d47cf33b300e6db416b173c68b
anuragarwalkar <anuragarwalkar@ehrdata.com>
2021-11-27 13:04:39 +0530

    added build and deploy script

--- src/Feed/StoryReel/Story/Story.js
+++ src/Feed/StoryReel/Story/Story.js
@@ -1,8 +1,14 @@
+import { Avatar } from "@mui/material";
 import React from "react";
 import "./story.scss";
 
-function Story() {
-  return <div className="story"></div>;
+function Story({ img, profileSrc, title }) {
+  return (
+    <div className="story">
+      <Avatar src={profileSrc} />
+      <h4>{title}</h4>
+    </div>
+  );
 }
 
 export default Story;
--- src/Feed/StoryReel/StoryReel.js
+++ src/Feed/StoryReel/StoryReel.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Story from "./Story/Story";
 import "./storyReel.scss";
 
 function StoryReel() {
-  return <div className="storyReel"></div>;
+  return (
+    <div className="storyReel">
+      <Story />
+    </div>
+  );
 }
 
 export default StoryReel;
