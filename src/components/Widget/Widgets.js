73c7353a268980f7285db72294000957da9aadc5
anuragarwalkar <anuragarwalkar@ehrdata.com>
2021-11-28 11:24:12 +0530

    added static width

--- src/components/Widget/Widgets.js
+++ src/components/Widget/Widgets.js
@@ -2,19 +2,7 @@ import React from "react";
 import "./Widgets.scss";
 
 function Widgets() {
-  return (
-    <div>
-      <iframe
-        title="anruag fb"
-        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fanurag.arwalkar%2F&tabs=timeline&width=340&height=100%25&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-        width="340"
-        height="100%"
-        style={{ border: "none" }}
-        scrolling="no"
-        samesite="None"
-      ></iframe>
-    </div>
-  );
+  return <div className="widgets"></div>;
 }
 
 export default Widgets;
